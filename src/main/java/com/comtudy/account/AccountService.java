b0ca7dfdf73d1f3ad96b326d61dd7e2dcae4a7cf
James Kim <kim9xd@gmail.com>
2021-08-05 00:22:53 +0900

    * 회원 가입   - 가입 확인 이메일 재전송

--- src/main/java/com/comtudy/account/AccountService.java
+++ src/main/java/com/comtudy/account/AccountService.java
@@ -49,7 +49,7 @@ public class AccountService {
 	}
 
 
-	private void sendSignUpConfirmEmail(Account newAccount) {
+	public void sendSignUpConfirmEmail(Account newAccount) {
 		SimpleMailMessage mailMessage = new SimpleMailMessage();
 		mailMessage.setSubject("스터디올래, 회원 가입 인증");
 		mailMessage.setText("/check-email-token?token=" + newAccount.getEmailCheckToken() + "&email=" + newAccount.getEmail());		
