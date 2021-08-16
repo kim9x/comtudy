c97d7069610ac2bcfaf71659c2a16b4b957d56bb
James Kim <kim9xd@gmail.com>
2021-08-17 02:27:27 +0900

    * 회원 가입   - 로그인 로그아웃

--- src/main/java/com/comtudy/account/AccountService.java
+++ src/main/java/com/comtudy/account/AccountService.java
@@ -9,6 +9,9 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,7 +22,7 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-public class AccountService {
+public class AccountService implements UserDetailsService {
 	
 	private final AccountRepository accountRepository;
 	private final JavaMailSender javaMailSender;
@@ -74,6 +77,20 @@ public class AccountService {
 //		 SecurityContext context = SecurityContextHolder.getContext();
 //		 context.setAuthentication(null);
 		
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String emailOrNickname) throws UsernameNotFoundException {
+        Account account = accountRepository.findByEmail(emailOrNickname);
+        if (account == null) {
+            account = accountRepository.findByNickname(emailOrNickname);
+        }
+
+        if (account == null) {
+            throw new UsernameNotFoundException(emailOrNickname);
+        }
+
+        return new UserAccount(account);
 	}
 
 }
