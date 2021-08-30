f07bef8dbb2cb1a0c4aa2454b892ff864adf3686
James Kim <kim9xd@gmail.com>
2021-08-30 18:14:38 +0900

    * 회원 가입   - 패스워드 수정

--- src/main/java/com/comtudy/account/AccountService.java
+++ src/main/java/com/comtudy/account/AccountService.java
@@ -109,4 +109,9 @@ public class AccountService implements UserDetailsService {
         // TODO 문제가 하나 더 남았습니다.
     }
 
+	public void updatePassword(Account account, String newPassword) {
+		account.setPassword(passwordEncoder.encode(newPassword));
+        accountRepository.save(account);
+	}
+
 }
